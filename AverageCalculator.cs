675f08094cf38a6ceecff4c2fa166cff833ac33f
franco <francotang2000@gmail.com>
2026-07-15 10:36:14 +0100

    Modify: edited code for averagewithdowhile

--- AverageCalculator.cs
+++ AverageCalculator.cs
@@ -3,7 +3,27 @@ internal class AverageCalculator
 {
     internal void AveragesWithDoWhile()
     {
-        
+        double total = 0.0;
+        int count = 0;
+        Console.Write("Enter the first number, or Q to quit: ");
+        string input = Console.ReadLine();
+        do
+        {
+            total += double.Parse(input);
+            count++;
+            Console.Write("Enter another number, or Q to quit: ");
+            input = Console.ReadLine();
+        }
+        while (input.ToUpper() != "Q") ;
+        if (count == 0)
+        {
+            Console.WriteLine("You didn't enter any numbers");
+        }
+        else
+        {
+            Console.WriteLine($"The average of those numbers is {total / count}");
+        }
+
     }
 
     internal void AveragesWithFor()
@@ -23,8 +43,14 @@ internal class AverageCalculator
             count++;
             Console.Write("Enter another number, or Q to quit: ");
             input = Console.ReadLine();
+        }
+        if (count == 0) 
+        {
+            Console.WriteLine("You didn't enter any numbers");
+        }
+        else
+        {
             Console.WriteLine($"The average of those numbers is {total / count}");
         }
-
     }
 }
